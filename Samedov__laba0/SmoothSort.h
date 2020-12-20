8f7893f5a19eca70e1bb2a444bfcc1313d65742e
Student803 <56917730+Student803@users.noreply.github.com>
2020-12-20 19:08:19 +0300

    Add files via upload

---
+++ Samedov__laba0/SmoothSort.h
@@ -0,0 +1,16 @@
+//
+//
+//
+//	//H1size= размер Heap1 и H2size= размер Heap2, Heap1size >= Heap2size
+//	int unsortedListLength, rightMostElementOfUnsortedList, leoHeapPlan, H1Size, H2Size, root1, baseStretch1, companionStretch1 = 0;
+//	std::vector<int> A;
+//	SmoothSort();
+//	~SmoothSort();
+//	void push(int);
+//	bool IsSorted(int, int);
+//	void Sift();
+//	void Trinkle();
+//	void SemiTrinkle();
+//	void SmoothSorting();
+//
+//#pragma once
